f1bd05643cc903486c392cfdf43baeb16cb51c85
RupomPB <rupombadhan111@gmail.com>
2025-09-25 21:53:45 +0600

    added navbar, banner and footer

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Container from "./components/Container";
+import Footer from "./components/Footer/Footer";
+
+import Navbar from "./components/Navbar/Navbar";
+
+
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+
+      {/* Banner */}
+      <Banner></Banner>
+      
+      {/* customer card */}
+    
+
+
+
+
+
+      {/*footer   */}
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Container from '../Container';
+
+
+const Banner = () => {
+    return (
+         <Container>
+        <div className=" grid grid-cols-2 justify-between gap-10 my-8">
+
+          {/* progress part */}
+          <div className="  h-[250px] bg-gradient-to-r from-[#632EE3] to-[#9F62F2] flex flex-col justify-center items-center rounded-2xl relative">
+
+          <div className=" absolute left-0 top-0 h-full">
+            <img src="/public/bg-left.png"></img>
+          </div>
+          <div className=" absolute right-0 top-0 h-full">
+            <img src="/public/bg-right.png"></img>
+          </div>
+            <h3 className=" font-bold text-4xl text-white">In-Progress</h3>
+            <p className=" font-semibold text-white text-6xl mt-3">0</p>
+          </div>
+
+
+          {/* resolved part */}
+          <div className="h-[250px] bg-gradient-to-r from-[#54CF68] to-[#00827A]  flex flex-col justify-center items-center rounded-2xl relative">
+
+          <div className=" absolute left-0 top-0 h-full">
+            <img src="/public/bg-left.png"></img>
+          </div>
+          <div className=" absolute right-0 top-0 h-full">
+            <img src="/public/bg-right.png"></img>
+          </div>
+            <h3 className=" font-bold text-4xl text-white">Resolved </h3>
+            <p className=" font-semibold text-white text-6xl mt-3">0</p>
+          </div>
+        </div>
+      </Container>
+    );
+};
+
+export default Banner;
